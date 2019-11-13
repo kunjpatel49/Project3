a35b4474bcb88f138be7386aff828510c6de4424
Kunj Patel <kpate331@uic.edu>
2019-11-13 05:13:41 +0000

    using screen buffer moving game piece by adding its velocity

--- main_javascript.js
+++ main_javascript.js
@@ -4,7 +4,8 @@ var canvas = document.querySelector("canvas");
 canvas.width = window.innerWidth - 200;
 canvas.height = window.innerHeight;
 var c = canvas.getContext('2d');
-
+var cWidth =  canvas.width;
+var cHeight = canvas.height;
 function component(width, height, color, x, y) {
     this.width = width;
     this.height = height;
@@ -12,11 +13,12 @@ function component(width, height, color, x, y) {
     this.y = y;
     c.fillStyle = color;
     c.fillRect(this.x, this.y, this.width, this.height);
-    this.interval = setInterval(updateGameArea, 20);
+
 }
 
 function startGame() {
-    myGamePiece = new component(30, 30, "red", canvas.width / 2, canvas.width / 2);
+    
+    myGamePiece = new component(30, 30, "red", cWidth / 2, cHeight / 2);
     gameObj = new component(200, 10, "green", canvas.width - 200, 0);
     gameobj = new component(200, 10, "green", 0, 0);
 
@@ -25,7 +27,9 @@ function startGame() {
 // Buffer Control
 updateGameArea();
 function updateGameArea() {
-    c.clearRect(0, 0, canvas.width, canvas.height);
+    requestAnimationFrame(updateGameArea);
+    c .clearRect(0, 0,canvas.width, canvas.height);
     startGame();
+    cHeight -= 1;
     console.log("buffer");
 }
