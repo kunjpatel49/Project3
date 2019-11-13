27a67042a955c08249e1f0d0886297b75ada03fa
Kunj Patel <kpate331@uic.edu>
2019-11-13 06:15:32 +0000

    game peice controlls added

--- main_javascript.js
+++ main_javascript.js
@@ -4,45 +4,66 @@ var canvas = document.querySelector("canvas");
 canvas.width = window.innerWidth - 200;
 canvas.height = window.innerHeight;
 var c = canvas.getContext('2d');
-var cWidth =  canvas.width;
+var cWidth = canvas.width;
 var cHeight = canvas.height;
 var objWidth = canvas.width;
 var objHeight = 0;
 var leftObjArray = [];
 var rightObjArray = [];
+var xVelocity = 0;
+var yVelocity = 0;
 myGamePiece = new component(30, 30, "red", cWidth / 2, cHeight / 2);
 
 function component(width, height, color, x, y) {
     this.width = width;
     this.height = height;
-    this.dX = 0;
-    this.dY = 0;
     this.x = x;
     this.y = y;
     c.fillStyle = color;
     c.fillRect(this.x, this.y, this.width, this.height);
-    this.newPos = function() {
-    this.x += this.speedX;
-    this.y += this.speedY;
-  }
-
 }
 
 function startGame() {
-    
     leftObjArray.push(new component(200, 10, "pink", 0, objHeight));
-    rightObjArray.push( new component(200, 10, "green", cWidth - 200, objHeight));
+    rightObjArray.push(new component(200, 10, "green", objWidth - 200, objHeight));
 }
-
 console.log(rightObjArray);
-
 // Buffer Control
 updateGameArea();
+
 function updateGameArea() {
     requestAnimationFrame(updateGameArea);
-    c .clearRect(0, 0,canvas.width, canvas.height);
+    c.clearRect(0, 0, canvas.width, canvas.height);
     myGamePiece = new component(30, 30, "red", cWidth / 2, cHeight / 2);
     startGame();
-    objHeight += 1;   // Moving Obstacles thorugh the canvas
+    objHeight += 1; // Moving Obstacles thorugh the canvas
     console.log("buffer");
+}
+
+// Controls
+function moveup() {
+    yVelocity -= 1;
+    cHeight += yVelocity;
+}
+
+function movedown() {
+    yVelocity += 1;
+    cHeight += yVelocity;
+}
+
+function moveleft() {
+    xVelocity -= 1;
+    cWidth += xVelocity;
+}
+
+function moveright() {
+    xVelocity += 1;
+    cWidth += xVelocity;
+}
+
+function stopMove() {
+    xVelocity = 0;
+    cWidth += xVelocity;
+    yVelocity = 0;
+    cHeight += yVelocity;
 }
