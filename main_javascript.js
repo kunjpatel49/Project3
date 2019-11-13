fc55b7344e3d75712bbd838f70e7098df3bd048e
Kunj Patel <kpate331@uic.edu>
2019-11-13 05:54:02 +0000

    moving the obstacles

--- main_javascript.js
+++ main_javascript.js
@@ -6,30 +6,43 @@ canvas.height = window.innerHeight;
 var c = canvas.getContext('2d');
 var cWidth =  canvas.width;
 var cHeight = canvas.height;
+var objWidth = canvas.width;
+var objHeight = 0;
+var leftObjArray = [];
+var rightObjArray = [];
+myGamePiece = new component(30, 30, "red", cWidth / 2, cHeight / 2);
+
 function component(width, height, color, x, y) {
     this.width = width;
     this.height = height;
+    this.dX = 0;
+    this.dY = 0;
     this.x = x;
     this.y = y;
     c.fillStyle = color;
     c.fillRect(this.x, this.y, this.width, this.height);
+    this.newPos = function() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
 
 }
 
 function startGame() {
     
-    myGamePiece = new component(30, 30, "red", cWidth / 2, cHeight / 2);
-    gameObj = new component(200, 10, "green", canvas.width - 200, 0);
-    gameobj = new component(200, 10, "green", 0, 0);
-
+    leftObjArray.push(new component(200, 10, "pink", 0, objHeight));
+    rightObjArray.push( new component(200, 10, "green", cWidth - 200, objHeight));
 }
+
+console.log(rightObjArray);
 
 // Buffer Control
 updateGameArea();
 function updateGameArea() {
     requestAnimationFrame(updateGameArea);
     c .clearRect(0, 0,canvas.width, canvas.height);
+    myGamePiece = new component(30, 30, "red", cWidth / 2, cHeight / 2);
     startGame();
-    cHeight -= 1;
+    objHeight += 1;   // Moving Obstacles thorugh the canvas
     console.log("buffer");
 }
